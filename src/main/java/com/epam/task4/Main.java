03f854088280da3ad518615d3ad2bb353653f055
ShivaniKolanu <shivanikolanu2000@gmail.com>
2020-02-19 18:49:39 +0530

    first commit

---
+++ src/main/java/com/epam/task4/Main.java
@@ -0,0 +1,10 @@
+package com.epam.task4;
+
+public class Main {
+	 public static void main( String[] args )
+	    {
+	    	Interest.calculateInterest();
+	        Construction.costEvaluator();
+	    }
+
+}
